022fed728a8c884f7fedb8b986aede9ab2008808
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-05-13 22:29:40 +0200

    Comienzo del ejercicio 3
    Aun no funciona :'(

---
+++ Ejercicio3.cc
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
+
+
+int main(int argc, char** argv) //argv[1] direccion, argv[2] puerti, argv[3] comando
+{
+    struct addrinfo infoaddres;
+    struct addrinfo * sockaddr, *aux;
+
+    memset((void*) &infoaddres, 0, sizeof(struct addrinfo));    //Reservamos memoria
+
+    //infoaddres.ai_flags = AI_PASSIVE;
+    infoaddres.ai_family = AF_INET; //Mira para IPv4 y para IPv6
+    infoaddres.ai_socktype = SOCK_DGRAM; //Puede ser TCP o UDP
+
+    //Probamos a ve si aparece la direccion en la red
+    int info = getaddrinfo(argv[1], argv[2], &infoaddres, &sockaddr); 
+
+    int sock;
+    
+    for(aux = sockaddr; aux != NULL; aux = aux->ai_next){    //Buscamos cual es una valida
+
+        sock = socket(aux->ai_family, aux->ai_socktype, 0); //creamos un socket
+        
+        if(sock == -1) 
+            continue;
+        if(connect(sock, aux->ai_addr, aux->ai_addrlen) != -1)
+            break;
+
+        close(sock); //Por si el encontramos no es valido
+    }
+
+    if(aux == NULL){
+        std::cout << "El socket no fue valido\n";
+        return -1;
+    }
+
+    freeaddrinfo(sockaddr); //Liberamos la informacion ya que ya no nos es necesario
+
+    bind(sock, sockaddr->ai_addr, sockaddr->ai_addrlen);
+    char buffer[80];
+
+    struct sockaddr client;
+    socklen_t clienteleng = sizeof(struct sockaddr);
+
+    sendto(sock, argv[3], sizeof(argv[3]), 0, NULL, 0); //Envio del comando dado
+    
+    int recievedBytes= recvfrom(sock, buffer, sizeof(buffer), 0, &client, & clienteleng);
+
+    //Comprobamos que llegue la informacion bien
+
+    return 0;
+}
