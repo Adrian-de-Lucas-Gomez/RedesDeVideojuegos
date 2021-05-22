101f61b8f9f4b19c1e959021ec09fbac2f57d59e
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-05-22 17:40:33 +0200

    Ejercicio 2 y 3 hechos
    Que el 3 es mirar que lo haga todo bien pero el caso es que ya están los 2 :D

--- Practica2.2/Ejercicio2/ejercicio1.cc
+++ Practica2.2/Ejercicio2/ejercicio1.cc
@@ -14,52 +14,101 @@ class Jugador: public Serializable
 public:
     Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
     {
-        strncpy(name, _n, 80);
+        strncpy(name, _n, MAX_LENGHT);
     };
 
     virtual ~Jugador(){};
 
     void to_bin()
     {
-        int fileDescriptor = open("SavedPlayer.txt", O_RDONLY | O_CREAT)
+        int32_t dataSize = sizePlayer();
+        alloc_data(dataSize);
 
-        if(fileDescriptor < 0){
-            std::cout << "Hubo un problema al abrir/crear el archivo\n";
-            return -1;
-        }
         //Escribimos primero la posicion en X Y
-        //write();
+        char *aux= _data;   //Lo guardamos en el array del jugador
+
+        memcpy(aux, &x, sizeof(int16_t));
+        //Movemos el puntero de escritura
+        aux += sizeof(int16_t);
+        memcpy(aux, &y, sizeof(int16_t));
+        //Movemos el puntero de escritura
+        aux += sizeof(int16_t);
 
         //Escribimos el nombre del player
-        //write();
+        memcpy(aux, name, sizeof(char)*MAX_LENGHT );
+        //Movemos el puntero de escritura
+        aux += sizeof(char)*MAX_LENGHT;
 
-        close(fileDescriptor);
+        
     }
 
     int from_bin(char * data)
     {
-        //
+        int32_t dataSize = sizePlayer();
+        alloc_data(dataSize);
+
+        //Ahora traemos la info de aux a name y X e Y
+
+        memcpy(&x, data, sizeof(int16_t));
+        //Movemos el puntero de escritura
+        data += sizeof(int16_t);
+        memcpy(&y, data, sizeof(int16_t));
+        //Movemos el puntero de escritura
+        data += sizeof(int16_t);
+
+        //Escribimos el nombre del player
+        memcpy(name, data, sizeof(char)*MAX_LENGHT);
+        //Movemos el puntero de escritura
+        data += sizeof(char)*MAX_LENGHT;
+
         return 0;
     }
 
 
 public:
-    char name[80];
+    static const size_t MAX_LENGHT=80;
+    char name[MAX_LENGHT];
 
     int16_t x;
     int16_t y;
+
+    int sizePlayer(){
+        int tam= (sizeof(char)*MAX_LENGHT) + (2 * sizeof(int16_t));
+        return tam;   
+    }
 };
 
 int main(int argc, char **argv)
 {
     Jugador one_r("", 0, 0);
-    Jugador one_w("Player_ONE", 123, 987);
+    Jugador one_w("Jugador 1", 164, 612);
 
+    int fileDescriptor = open("./SavedPlayer", O_TRUNC | O_CREAT | O_RDWR, 0666);
+
+    if(fileDescriptor < 0){
+        std::cout << "Hubo un problema al abrir/crear el archivo\n";
+        return -1;
+    }
+
+    
     // 1. Serializar el objeto one_w
+    one_w.to_bin();
+
     // 2. Escribir la serialización en un fichero
+    write(fileDescriptor, one_w.data(), one_w.sizePlayer());
+    close(fileDescriptor);
+
     // 3. Leer el fichero
+    fileDescriptor = open("./SavedPlayer", O_RDONLY);
+    char *deserializedBuffer= (char *)malloc(one_r.sizePlayer());
+    read(fileDescriptor, deserializedBuffer, one_r.sizePlayer());
+    close(fileDescriptor);
+
     // 4. "Deserializar" en one_r
+    one_r.from_bin(deserializedBuffer);
+
     // 5. Mostrar el contenido de one_r
+    std:: cout << "Player name: " << one_r.name << " X: " << one_r.x << " Y: " << one_r.y << std::endl;
 
     return 0;
 }
