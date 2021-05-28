e54c4142d105cfcf181e595d799bcd43d3274cf2
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-05-28 14:16:00 +0200

    Ejercicio6
    Preparado el bucle de recepción de los mensajes y según el tipo hace una cosa u otra

--- Practica2.2/Ejercicio4-8/Chat.cc
+++ Practica2.2/Ejercicio4-8/Chat.cc
@@ -68,9 +68,51 @@ void ChatServer::do_messages()
          */
 
         //Recibir Mensajes en y en función del tipo de mensaje
+        ChatMessage mensaje;
+        Socket *sock= &socket;
+
+        socket.recv(mensaje, sock);
+
         // - LOGIN: Añadir al vector clients
         // - LOGOUT: Eliminar del vector clients
         // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
+        switch(mensaje.type){
+            case 0:
+                std::cout << "Recibido LOGGIN de " << mensaje.nick << "\n";
+                std::unique_ptr<Socket> cliente(sock);
+                clients.push_back(std::move(cliente));
+            break;
+
+            case 1:
+                std::cout << "Recibido MESSAGE de " << mensaje.nick << "\n";
+                for(int i=0; i<clients.size; i++){
+                    Socket aux = *clients[i].get();
+
+                    if(!(aux == *sock)){    //Si no es la misma persona se lo reenvio
+                        socket.send(mensaje, aux);
+                    }
+                }
+            break;
+
+            case 2:
+                std::cout << "LOGOUT de " << mensaje.nick << "\n";
+
+                bool desconexionRealizada=false;
+                int i=0;
+
+                while(!desconexionRealizada && i < clients.size()){
+                    Socket aux = *clients[i].get();
+
+                    if(aux == *sock){    //Si no es la misma persona se lo reenvio
+                        socket.send(mensaje, aux);
+                        clients.erase(clients.begin()+i);
+                        desconexionRealizada = true;
+                    }
+
+                    i++;
+                }
+            break;
+        }
     }
 }
 
